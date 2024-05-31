9e47e8d6ba26df56bbc468034db7819bb92b5a1a
Mariana Montenegro <mmontenegro@invertironline.com>
2024-05-31 20:30:08 -0300

    Update Project

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,24 +1,18 @@
-import com.google.common.annotations.VisibleForTesting;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.*;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 
 public class SeleniumTest {
     private static final Logger logger = LogManager.getLogger(SeleniumTest.class);
